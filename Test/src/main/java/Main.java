0ea533e9a93165d55a479663ec4357c0b5d7fc71
KateLegion <SurEks18@gmail.com>
2022-09-05 11:08:32 +0300

    Beaty

--- Test/src/main/java/Main.java
+++ Test/src/main/java/Main.java
@@ -10,14 +10,10 @@ public class Main {
         File file = new File("src/Задача ВС Java Сбер.csv");
 
         try (Scanner sc = new Scanner(file, StandardCharsets.UTF_8)) {
-            for (int i = 0; i < 7; i++) {
+            for (int i = 0; i < 1100; i++) {
                 String text = sc.nextLine();
                 String[] words = text.split(";");
 
-                //for (int j = 0; j < words.length; j++) {
-                    //System.out.println(words[j]);
-                //}
-                //String[] array = {words[0], words[1], words[2], words[3], words[4], words[5]};
                 City tmp = new City ();
                 tmp.setName(words[1]);
                 tmp.setRegion(words[2]);
